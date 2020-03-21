6df7f5b36d60b618b4fd2beba11d24199e320776
xxCallum <callumsettergren@gmail.com>
2020-03-21 11:27:48 -0700

    hello world

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+//section 10 hello world
+//print hello world
+
+public class helloworld
+{
+  //main method begins execution of Java application
+  public static void main(String[] args)
+  {
+    /** @return nothing
+      * @param String */
+    System.out.println("Callum is awesome!");
+  } //end main method
+} //end welcome class
