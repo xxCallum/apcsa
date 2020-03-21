626601c4feb0d97f0d8b0eaf559b0a502b44e63b
xxCallum <callumsettergren@gmail.com>
2020-03-21 12:17:09 -0700

    section 18

---
+++ modifiedhelloworld.java
@@ -0,0 +1,14 @@
+//section 18 hello world
+//print variations of hello world
+
+public class modifiedhelloworld
+{
+  //main method begins execution of Java application
+  public static void main(String[] args)
+  {
+    /** @return nothing
+      * @param String */
+    int number = 100;
+    System.out.println(number);
+  } //end main method
+} //end welcome class
