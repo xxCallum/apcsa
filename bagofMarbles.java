19afdcfd0744aedb1320e4a7bf0af4e972284de8
xxCallum <callumsettergren@gmail.com>
2020-04-02 16:36:13 -0700

    section 10 AND 11 tingz

---
+++ bagofMarbles.java
@@ -0,0 +1,14 @@
+//section 11 marblez
+
+public class bagofMarbles
+{
+  public static void main(String[] args)
+  {
+    String[] bag = {"red", "blue", "green", "white", "black"};
+    System.out.println(bag[0]);
+    System.out.println(bag[1]);
+    System.out.println(bag[2]);
+    System.out.println(bag[3]);
+    System.out.println(bag[4]);
+  }
+}
---
+++ twodarray.java
@@ -0,0 +1,16 @@
+//NOT AN ASSIGNMENT
+
+public class twodarray
+{
+  public static void main(String[] args)
+  {
+    int[][] x = {{3,4,5},{4,9,1}, {7,8,2}};
+    // 3 4 5
+    // 4 9 1
+    // 7 8 2
+    
+    System.out.println(x[2][2]);
+  }
+}
+
+                            
