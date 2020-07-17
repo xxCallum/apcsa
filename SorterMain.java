564edb4812fc8700aef9b40308b54b4047225c49
Callum <callumsettergren@gmail.com>
2020-07-17 14:08:26 -0700

    all the things i havent done

--- SorterMain.java
+++ SorterMain.java
@@ -2,16 +2,13 @@ public class SorterMain {
   public static void main(String[] args) { 
     SorterBubble bubbly = new SorterBubble(10);  // construct a bubble-sorter with 1..10 scrambled
     bubbly.print(true);                          // print the state of the sorter (true-->and also all 10 numbers)
-    bubbly.indexSwap(1,3);
-    bubbly.print(true);
     bubbly.sort();                               // perform a bubble sort to put the numbers in order
-    bubbly.newDecreasing(7);
     bubbly.print(true);                          // print the state again, as well as the sorted numbers
     
     /*SorterQuick quickly = new SorterQuick(10);   // construct a quick-sorter with 1..10 scrambled
     quickly.print(true);                         // ...
     quickly.sort();
-    quickly.print(true);
+    quickly.print(true);*/
     
     
     // Now we'll exercise the sorters on 100, 200, 300, ... 1000 numbers
@@ -21,7 +18,7 @@ public class SorterMain {
       bubbly.print(false);                       // print the state but not the numbers
     }
    
-    for (int n=100; n<=1000; n+=100) {           // same for quick-sort
+    /*for (int n=100; n<=1000; n+=100) {           // same for quick-sort
       quickly = new SorterQuick(n);
       quickly.sort();
       quickly.print(false);
